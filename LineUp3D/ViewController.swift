779fc42e7a4d6003934da38970121ab7f5f64627
Mauricio Figueroa <m.figueroa.olivares@live.cl>
2019-02-25 23:23:33 -0300

    Initial Commit

---
+++ LineUp3D/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LineUp3D
+//
+//  Created by Mauricio Figueroa olivares on 2/25/19.
+//  Copyright © 2019 Mauricio Figueroa olivares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
